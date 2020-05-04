53a56857414d9f3171738ce93ffb642cf7e5bf97
Krzysiek Niżnik <chrisphilipniznik@gmail.com>
2020-05-05 00:17:49 +0200

    Exercise 1 presumably finished, additional patches may be required

--- task-1/form_rows.js
+++ task-1/form_rows.js
@@ -1,5 +1,37 @@
 function main() {
     // event handlers and other code here
+    const add = document.getElementById('add_row');
+    const container = document.querySelector('.container');
+    // let counter = 1;
+    let listRows = [1];
+    add.addEventListener('click', function() {
+
+        if(listRows.length === 10){
+            alert('ONLY 10 ROWS!');
+        }else{
+        const form = document.createElement('form');
+        listRows.push(form);
+        form.method = "GET";
+        const label = document.createElement('label');
+        label.htmlFor = "item_"+(listRows.length);
+        label.textContent = "Item " + (listRows.length)+ ':';
+        form.appendChild(label);
+        const input = document.createElement('input');
+        input.type = "text";
+        input.name = "item_"+(listRows.length+1);
+        input.id = "item_"+(listRows.length+1);
+        form.appendChild(input);
+        const cancel = document.createElement('button');
+        cancel.textContent = "BUTTON";
+        cancel.addEventListener('click', function() {
+            container.removeChild(form);
+            listRows.pop(form);
+        });
+        form.appendChild(cancel);
+        container.appendChild(form);
+
+        }
+    });
 }
 
 main();
