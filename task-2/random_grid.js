5b7d0754309f1914d140990e96a6b23fbddd2321
Krzysiek Niżnik <chrisphilipniznik@gmail.com>
2020-05-08 12:37:12 +0200

    Refactor code from task 1 and finish task 3

--- task-2/random_grid.js
+++ task-2/random_grid.js
@@ -1,31 +1,33 @@
-function main() {
-    // event handlers and other code here
-    let container = document.querySelector('.container');
-
-    for (let i = 0; i < 10; i++) {
-        let row = document.createElement('div');
-        row.className = 'row';
-        for (let j = 1; j <= 10; j++) {
-            let cell = document.createElement('div');
-            cell.className = "cell";
-            cell.textContent = (i * 10) + j;
-            row.appendChild(cell);
-        }
-        container.appendChild(row);
-    }
-
-    let cells = document.getElementsByClassName("cell");
-
+function switchCellColor() {
+    // event handlers and other code here 
+    const cells = document.getElementsByClassName("cell");
     setInterval(function () {
         let random = Math.floor(Math.random() * (cells.length));
         cells[random].classList.add("active");
-        setTimeout(function () {
-            cells[random].classList.remove("active");
-        },500)
+        setTimeout(()=>setActive(cells,random), 500)
         // cells[random].className -= " active";
         // delete cells[random].className;
     }, 500);
 }
 
+function setActive(cells,random) {
+    cells[random].classList.remove("active");
+}
 
-main();
+function generateGrid(width,height) {
+    let container = document.querySelector('.container');
+
+    for (let i = 0; i < height; i++) {
+        let row = document.createElement('div');
+        for (let j = 1; j <= width; j++) {
+            let cell = document.createElement('div');
+            cell.className = "cell";
+            cell.textContent = (i * height) + j;
+            row.appendChild(cell);
+        }
+        container.appendChild(row);
+    }
+}
+
+generateGrid(10,10);
+switchCellColor();
